4b849a276bf501beae0d2f62605268cb66989520
Queen-Geek <QueenGeekWorld@gmail.com>
2022-02-16 01:29:04 -0500

    Fixed the decimal point error. Now 2 decimals appear

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 print("Welcome to the tip calculator.\n")
 
 
-total = int(input("What was the total of your bill?\n"))
+total = float(input("What was the total of your bill?\n"))
 percentage = float(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
 split = int(input("How many people are going to split the bill?\n"))
 
-convertedPercentage = percentage / 100
+convertedPercentage = round((percentage / 100), 2)
 
-billTotal = (total * convertedPercentage + total) / split
+billTotal = "{:.2f}".format((total * convertedPercentage + total) / split)
+
+
 
 print(f"Your total is {billTotal} per person")
